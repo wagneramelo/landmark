306972b1b488d19ef6e91127b781b7e09eedf47c
Wagner Melo <wagnermelo@me.com>
2023-07-01 01:43:15 -0300

    missing files

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Wagner Augusto Melo de Paulo on 26/06/23.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Wagner Augusto Melo de Paulo on 26/06/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
